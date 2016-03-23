e19c48319966b0145349420bb697ae17751c7a80
Joe Wilm <joe@jwilm.com>
2016-03-23 09:49:38 -0700

    Stub out basic API
    Lots of APIs are stringly-typed at this point, but they will eventually
    have stronger typing (Urls and Paths, for example). The response is also
    fully buffered within hydra at the moment. The request handler should
    require more methods in the future to move data streaming to the
    consumer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,110 @@
-#[cfg(test)]
-mod test {
-    #[test]
-    fn it_works() {
+//! Hydra - An evented HTTP/2 client library
+//!
+extern crate mio;
+extern crate solicit;
+extern crate hyper;
+
+use std::marker::PhantomData;
+
+// Would be cool if these got put into separate crates
+pub use hyper::method::Method;
+pub use hyper::status::{StatusCode, StatusClass};
+pub use hyper::header::{self, Headers};
+
+#[cfg(feature = "tls")]
+extern crate openssl;
+
+pub trait ConnectionHandler: Send + 'static {
+    fn on_connection(&self);
+    fn on_error(&self, err: ConnectionError);
+    fn on_pong(&self);
+}
+
+pub trait RequestHandler: Send + 'static {
+    fn on_error(&self, err: RequestError);
+    fn on_response(&self, res: Response);
+}
+
+pub trait Connector: Send + 'static {}
+
+pub struct Config {
+    pub threads: u8,
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config { threads: 8 }
     }
+}
+
+pub struct Hydra<H: ConnectionHandler> {
+    _handler: H
+}
+
+impl<H> Hydra<H>
+    where H: ConnectionHandler
+{
+    pub fn new(_config: &Config) -> Hydra<H> {
+        unimplemented!();
+    }
+
+    pub fn connect<'a, U>(&'a self, _url: U, _handler: H) -> Client<'a>
+        where U: Into<String>
+    {
+        unimplemented!();
+    }
+
+    #[cfg(feature = "tls")]
+    pub fn connect_tls<'a, U>(&'a self, _url: U, _handler: H) -> Client<'a>
+        where U: Into<String>
+    {
+        unimplemented!();
+    }
+
+    pub fn connect_with<'a, U, C>(&'a self, _url: U, _handler: H, _connector: C) -> Client<'a>
+        where C: Connector,
+              U: Into<String>
+    {
+        unimplemented!();
+    }
+}
+
+pub struct Client<'a> {
+    _marker: PhantomData<&'a u8>
+}
+
+impl<'a> Client<'a> {
+    pub fn request<H>(&self, _req: Request, handler: H) {
+        let _handler = Box::new(handler);
+        unimplemented!();
+    }
+}
+
+pub struct Request {
+    _method: Method,
+    _path: String,
+    _body: Vec<u8>,
+}
+
+impl Request {
+    pub fn new<P, B>(method: Method, path: P, body: B) -> Request
+        where P: Into<String>,
+              B: Into<Vec<u8>>,
+    {
+        Request {
+            _method: method,
+            _path: path.into(),
+            _body: body.into(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Response;
+
+pub enum ConnectionError {
+    Variant
+}
+pub enum RequestError {
+    Variant
 }
---
+++ tests/lib.rs
@@ -0,0 +1,132 @@
+extern crate hydra;
+
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::mpsc;
+
+use hydra::{Method, Request, Hydra};
+
+enum ConnectionMsg {
+    Error(hydra::ConnectionError),
+    Connected,
+    Pong
+}
+
+struct ConnectHandler {
+    tx: mpsc::Sender<ConnectionMsg>,
+}
+
+impl ConnectHandler {
+    pub fn new(tx: mpsc::Sender<ConnectionMsg>) -> Self {
+        ConnectHandler { tx: tx }
+    }
+}
+
+impl hydra::ConnectionHandler for ConnectHandler {
+    fn on_connection(&self) {
+        self.tx.send(ConnectionMsg::Connected).unwrap();
+    }
+
+    fn on_error(&self, err: hydra::ConnectionError) {
+        self.tx.send(ConnectionMsg::Error(err)).unwrap();
+    }
+
+    fn on_pong(&self) {
+        self.tx.send(ConnectionMsg::Pong).unwrap();
+    }
+}
+
+fn response_collector() -> (RequestHandler, ResponseCollector) {
+    let (tx, rx) = mpsc::channel();
+    let counter = Arc::new(AtomicUsize::new(1));
+    let handler = RequestHandler::new(tx, counter.clone());
+    let collector = ResponseCollector::new(rx, counter);
+
+    (handler, collector)
+}
+
+struct ResponseCollector {
+    rx: mpsc::Receiver<Option<hydra::Response>>,
+    counter: Arc<AtomicUsize>,
+}
+
+impl ResponseCollector {
+    pub fn new(rx: mpsc::Receiver<Option<hydra::Response>>, counter: Arc<AtomicUsize>)
+        -> ResponseCollector
+    {
+        ResponseCollector { rx: rx, counter: counter}
+    }
+
+    pub fn wait_all(&self) {
+        while self.counter.load(Ordering::SeqCst) != 0 {
+            let msg = self.rx.recv().unwrap();
+            println!("resp: {:?}", msg);
+        }
+    }
+}
+
+struct RequestHandler {
+    tx: mpsc::Sender<Option<hydra::Response>>,
+    counter: Arc<AtomicUsize>,
+}
+
+impl Clone for RequestHandler {
+    fn clone(&self) -> RequestHandler {
+        self.counter.fetch_add(1, Ordering::SeqCst);
+        RequestHandler {
+            tx: self.tx.clone(),
+            counter: self.counter.clone(),
+        }
+    }
+}
+
+impl RequestHandler {
+    pub fn new(tx: mpsc::Sender<Option<hydra::Response>>,
+               counter: Arc<AtomicUsize>) -> RequestHandler
+    {
+        RequestHandler { tx: tx, counter: counter, }
+    }
+}
+
+impl hydra::RequestHandler for RequestHandler {
+    fn on_error(&self, _err: hydra::RequestError) {
+        self.counter.fetch_sub(1, Ordering::SeqCst);
+        self.tx.send(None).unwrap();
+    }
+
+    fn on_response(&self, res: hydra::Response) {
+        self.counter.fetch_sub(1, Ordering::SeqCst);
+        self.tx.send(Some(res)).unwrap();
+    }
+}
+
+#[test]
+fn send_request() {
+    let mut config = hydra::Config::default();
+    config.threads = 1;
+
+    let (tx, rx) = mpsc::channel();
+    let handler = ConnectHandler::new(tx);
+
+    let cluster = Hydra::new(&config);
+    let client = cluster.connect("http://http2bin.org", handler);
+
+    // Wait for the connection.
+    match rx.recv().unwrap() {
+        ConnectionMsg::Connected => (),
+        _ => panic!("Didn't recv Connected"),
+    }
+
+    let (req_handler, collector) = response_collector();
+
+    let req = Request::new(Method::Get, "/get", "");
+    client.request(req, req_handler.clone());
+
+    let req = Request::new(Method::Get, "/get", "");
+    client.request(req, req_handler.clone());
+
+    let req = Request::new(Method::Get, "/get", "");
+    client.request(req, req_handler);
+
+    collector.wait_all();
+}
